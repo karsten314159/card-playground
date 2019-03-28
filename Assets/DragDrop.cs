c72c782aa22cfb10905744fefbf82ae744744e3a
Karsten <brox.p@web.de>
2019-03-28 23:54:06 +0100

    import

---
+++ Assets/DragDrop.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** https://gist.github.com/YoungjaeKim/6045432 */
+public class DragDrop : MonoBehaviour
+{  
+	private bool _mouseState;
+	public GameObject Target;
+	public Vector3 screenSpace;
+	public Vector3 offset;
+
+	private float dist = 0.25f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Target = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Debug.Log(_mouseState);
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hitInfo;
+			if (Target == GetClickedObject (out hitInfo)) {
+				_mouseState = true;
+				screenSpace = Camera.main.WorldToScreenPoint (Target.transform.position);
+				offset = Target.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+			}
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			_mouseState = false;
+		}
+		var dir =Vector3.zero;
+			var trans = Target.transform;
+		if (_mouseState) {
+			//keep track of the mouse position
+			var curScreenSpace = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+
+			//convert the screen mouse position to world point and adjust with offset
+			var curPosition = Camera.main.ScreenToWorldPoint (curScreenSpace) + offset;
+
+			 dir = curPosition - trans.position;
+			//update the position of the object in the world
+			trans.position = curPosition;
+		}
+
+        // Hover
+		var asd = trans.position;
+		asd.z = _mouseState ? -dist : 0;
+		trans.position = asd;
+
+		// https://answers.unity.com/questions/46845/face-forward-direction-of-movement.html#
+		 trans.rotation = Quaternion.Slerp(
+	        trans.rotation,
+	        Quaternion.LookRotation(dir),
+	        Time.deltaTime * 3f
+	    );
+	}
+	
+	
+	GameObject GetClickedObject (out RaycastHit hit)
+	{
+		GameObject target = null;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray.origin, ray.direction * 10, out hit)) {
+			target = hit.collider.gameObject;
+		}
+
+		return target;
+	}
+}
---
+++ Assets/Texture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Texture : MonoBehaviour
+{  
+	public string name;
+
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		var url = "http://gatherer.wizards.com/Handlers/Image.ashx?type=card&name=" + name;
+		
+        // Start a download of the given URL
+        using (WWW www = new WWW(url))
+        {
+            // Wait for download to complete
+            yield return www;
+
+            // assign texture
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = www.texture;
+        }
+	}
+	
+}
